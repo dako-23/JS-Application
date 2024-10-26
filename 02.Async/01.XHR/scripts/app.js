82de261c8ed9c39aacb2a926a99f3ad57b5a2c0f
dako-23 <iordan.megaparts@gmail.com>
2024-10-26 12:52:56 +0300

    .

--- 02.Async/01.XHR/scripts/app.js
+++ 02.Async/01.XHR/scripts/app.js
@@ -1,3 +1,9 @@
 function loadRepos() {
-   console.log("TODO...");
+   let url = 'https://api.github.com/users/testnakov/repos';
+
+   fetch(url)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+
 }
