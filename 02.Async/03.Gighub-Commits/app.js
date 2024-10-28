dc53253d47a7752e134776b1e2aae65957cca445
dako-23 <iordan.megaparts@gmail.com>
2024-10-28 13:51:16 +0200

    .

--- 02.Async/03.Gighub-Commits/app.js
+++ 02.Async/03.Gighub-Commits/app.js
@@ -1,4 +1,38 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+
+    const userNameEl = document.querySelector('#username').value;
+    const repoEl = document.querySelector('#repo').value;
+    const commitEl = document.querySelector('#commits');
+
+    const url = `https://api.github.com/repos/${userNameEl}/${repoEl}/commits`;
+
+    fetch(url)
+        .then(res => {
+
+            if (!res.ok) {
+                throw new Error(`Eror: ${res.status} (Not Found)`);
+            }
+
+            return res.json()
+        })
+        .then(repos => {
+            commitEl.innerHTML = '';
+
+            repos.forEach(repo => {
+                const li = document.createElement('li');
+                li.textContent = `${repo.commit.author.name}: ${repo.commit.message}`
+
+                commitEl.appendChild(li)
+            });
+        })
+        .catch(err => {
+            commitEl.innerHTML = '';
+            const errLi = document.createElement('li');
+            errLi.textContent = err.message
+            commitEl.appendChild(errLi)
+
+        });
+
+
+
 }
