c6b2331e2312fa47f9f6c1866a82d31d41c0f40a
dako-23 <iordan.megaparts@gmail.com>
2024-10-29 10:27:43 +0200

    .

--- 02.Async-Exercise/02.Bus-Schedule/app.js
+++ 02.Async-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,39 @@
 function solve() {
 
+    const infoEl = document.querySelector('.info');
+    const departBtn = document.querySelector('#depart')
+    const arriveBtn = document.querySelector('#arrive')
+
+    let stop = {
+        next: 'depot'
+    }
+
+
     function depart() {
-        console.log('Depart TODO...');
+
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(res => {
+                console.log(res);
+
+                stop = res
+                infoEl.textContent = `Next stop ${stop.name}`
+
+            })
+            .catch(err => console.log(err));
+        departBtn.disabled = true
+        arriveBtn.disabled = false
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+
+        infoEl.textContent = `Arriving at ${stop.name}`
+        departBtn.disabled = false
+        arriveBtn.disabled = true
+
     }
 
     return {
