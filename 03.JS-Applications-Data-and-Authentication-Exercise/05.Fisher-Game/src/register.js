29068c3c1d1cd0b12c3102fe52a1c2178da77c70
dako-23 <iordan.megaparts@gmail.com>
2024-11-05 11:58:50 +0200

    .

--- 03.JS-Applications-Data-and-Authentication-Exercise/05.Fisher-Game/src/register.js
+++ 03.JS-Applications-Data-and-Authentication-Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,28 @@
-console.log('TODO:// Implement Register functionality');
+const registerForm = document.querySelector('#register')
+const urlReg = 'http://localhost:3030/users/register'
+
+registerForm.addEventListener('submit', createAcc);
+
+async function createAcc(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const res = await fetch(urlReg, {
+        method: 'POST',
+        body: JSON.stringify({
+            email: formData.get('email'),
+            password: formData.get('password')
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const data = res.json();
+
+    localStorage.setItem('accessToken', data.accessToken);
+    localStorage.setItem('email', data.email);
+
+    location.href = '/';
+
+}
